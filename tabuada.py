b4f231218590f99b9c98b26eb31d8bde13e17f79
VstVictor <torresvictor.silva@gmail.com>
2023-11-27 15:19:37 -0300

    Tabuada

---
+++ tabuada.py
@@ -0,0 +1,38 @@
+# tabuada para vc que tem preguiça de decorar
+
+# apresentação
+
+print('\n\t\t\t -- Tabuada --\n')
+
+# entrada
+
+# informe a tabuada
+
+num = int(input ('informe a tabuada:'));
+
+# processamento
+
+n1 = num * 1
+n2 = num * 2
+n3 = num * 3
+n4 = num * 4
+n5 = num * 5
+n6 = num * 6
+n7 = num * 7
+n8 = num * 8
+n9 = num * 9
+n10 = num * 10
+
+
+# saída
+
+print(f'{num} x {1} = {n1}')
+print(f'{num} x {2} = {n2}')
+print(f'{num} x {3} = {n3}')
+print(f'{num} x {4} = {n4}')
+print(f'{num} x {5} = {n5}')
+print(f'{num} x {6} = {n6}')
+print(f'{num} x {7} = {n7}')
+print(f'{num} x {8} = {n8}')
+print(f'{num} x {9} = {n9}')
+print(f'{num} x {10} = {n10}')
